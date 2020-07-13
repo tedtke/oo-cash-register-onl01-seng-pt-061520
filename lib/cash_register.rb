e3a66c44ecc76bfedc41185b1f704153b10eecda
Teodoro Garcia III <tedtke514@gmail.com>
2020-07-13 18:16:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,34 +9,34 @@ class CashRegister
   end
 
 
-  # def add_item(title,price,quantity = 1)
-  #   if quantity>1
-  #     i=0
-  #     while i<quantity
-  #       @items << title
-  #       i+=1
-  #     end
-  #   else
-  #     @items << title
-  #   end
-  #   @total += price*quantity
-  #   @last_transaction_amount = @total
-  #   @total
-  # end
+  def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
+    else
+      @items << title
+    end
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
+  end
 
-  # def apply_discount()
-  #   if @discount > 0
-  #     @discount = @discount/100.to_f
-  #     @total = @total - (@total * (@discount))
-  #     "After the discount, the total comes to $#{@total.to_i}."
-  #   else
-  #     "There is no discount to apply."
-  #   end
-  # end
+  def apply_discount()
+    if @discount > 0
+      @discount = @discount/100.to_f
+      @total = @total - (@total * (@discount))
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
 
 
-  # def void_last_transaction()
-  #   @total -= @last_transaction_amount
-  # end
+  def void_last_transaction()
+    @total -= @last_transaction_amount
+  end
 
 end
